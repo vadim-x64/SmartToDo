38f7dbee9dd8021cae2b2b47b55044db88f43aba
Vadym <vadim.rolex.2005@gmail.com>
2025-11-17 22:03:00 +0200

    Added url for connection.

--- src/config/db.js
+++ src/config/db.js
@@ -9,6 +9,7 @@ const pool = new Pool({
     // database: process.env.DB_NAME,
     // password: process.env.DB_PASSWORD,
     // port: process.env.DB_PORT,
+
 });
 
 pool.on('connect', (client) => {
