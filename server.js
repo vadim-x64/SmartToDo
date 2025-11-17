02a52430b804d6a7a5a8f73fbb77c8b65fc946ef
Vadym <vadim.rolex.2005@gmail.com>
2025-11-17 21:48:40 +0200

    Trying to deploy first version on special service.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
+        secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000
     }
--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,13 @@ const {Pool} = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
 });
 
 pool.on('connect', (client) => {
