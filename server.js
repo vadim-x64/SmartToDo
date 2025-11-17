735f99fc1d3ba4f6a5bc0fcd6fe3515b8f06dc7f
Vadym <vadim.rolex.2005@gmail.com>
2025-11-17 22:14:04 +0200

    Returned to previous version without dockerfile.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000
     }
